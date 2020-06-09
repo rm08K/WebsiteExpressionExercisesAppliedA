651c0b49d893ab9914bbcea60676158f25cac080
rm08K <a18dc175@dhw.ac.jp>
2020-06-10 00:34:10 +0900

    add final work

---
+++ docs/js/swiper.js
@@ -0,0 +1,32 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    speed: 600,
+    slidesPerView: 1.8,
+    spaceBetween:0,
+    centeredSlides: true,
+    direction: 'horizontal',
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate:0,
+        slidesShadows: false,
+        depth: 0,
+        modifier: 0,
+    },
+    autoplay: {
+        delay: 4000, 
+        stopOnLast: false,
+        disableOnInteraction: true
+    },
+    980: {
+        slidesPerView: 1,
+    }
+})
